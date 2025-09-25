87211e6ee745aa3f4ac7c746aae7162d3b0f835b
jaswinderkaurr <jnirmaan22@gmail.com>
2025-09-25 13:05:06 -0400

    Create utils.py

---
+++ backend/utils.py
@@ -0,0 +1,12 @@
+from torchvision import transforms
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+
+def get_transform():
+    """Return preprocessing transform for ResNet."""
+    return transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+    ])
